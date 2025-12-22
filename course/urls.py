cb62d9b2147d81361463eda1093572aa49669688
KNR1997 <kethakaranasinghe@gmail.com>
2025-12-22 23:06:47 +0530

    feat(urls): Add CourseOffering endpoints for listing, creating, retrieving, updating, and deleting course offerings

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from course.views import CourseViewSet
+from course.views.course import CourseViewSet
+from course.views.course_offering import CourseOfferingViewSet
 
 router = DefaultRouter()
 
@@ -21,5 +22,20 @@ urlpatterns = [
             "delete": "destroy",
         }),
         name="course",
+    ),
+    path(
+        "course-offerings/",
+        CourseOfferingViewSet.as_view({"get": "list", "post": "create"}),
+        name="course-offering",
+    ),
+    path(
+        "course-offerings/<str:slug>/",
+        CourseOfferingViewSet.as_view({
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        }),
+        name="course-offering",
     ),
 ]
